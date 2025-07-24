86f5c50da8d61e014975da96d636347160146ec8
user.name : "Jay Kishan <user.email:"jkn18869!@gmail.com>
2025-07-25 04:18:23 +0530

    node modules add in git ignore

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,16 @@ import { BrowserRouter } from 'react-router'
 import 'remixicon/fonts/remixicon.css'
 import { ProductProvider } from './context/ProductContext.jsx'
 import { SlidesProvider } from './context/Slidescontext';
-import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
+
+
 createRoot(document.getElementById('root')).render(
   <ProductProvider>
     <SlidesProvider>  
     <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+   
       <App />
-    </ClerkProvider>
+   
     </BrowserRouter>
     </SlidesProvider>
   </ProductProvider>,
